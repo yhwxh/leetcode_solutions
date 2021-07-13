f0e3ef7a7fcc8e05209aff651849892ed9ada3b5
wxhyh <emtf>
2021-07-14 00:23:40 +0800

    complete BST

--- src/data_sturcture/BinarySearchTree.java
+++ src/data_sturcture/BinarySearchTree.java
@@ -1,7 +1,5 @@
 package data_sturcture;
 
-import utils.ListNode;
-
 import java.util.*;
 import java.util.LinkedList;
 
@@ -154,10 +152,10 @@ public class BinarySearchTree<E extends Comparable> {
             System.out.print(cur.val + " ");
             // 对当前节点的孩子节点压栈的顺序是先右后左
             if (cur.right != null){
-                elements.add(cur.left);
-            }
-            if (cur.right !=null){
                 elements.add(cur.right);
+            }
+            if (cur.left !=null){
+                elements.add(cur.left);
             }
         }
     }
@@ -214,8 +212,10 @@ public class BinarySearchTree<E extends Comparable> {
         }
         return null;
     }
-    public Node removeMin(){
-        return removeMin(root);
+    public E removeMin(){
+        E minE = minimum(root).val;
+        removeMin(root);
+        return minE;
     }
     private Node removeMin(Node node){
         /**
@@ -230,16 +230,18 @@ public class BinarySearchTree<E extends Comparable> {
             return rightChildren;
         }
         node.left = removeMin(node.left);  // 递归，并将右子树接在左子树的位置
-        return node;  // 返回当前新树
+        return node;  // 返回当前新树的根
     }
     private Node minimum(Node node){
         if (node.left == null) {
-            return node.left;
+            return node;
         }
         return minimum(node.left);
     }
-    public Node removeMax() {
-        return removeMax(root);
+    public E removeMax() {
+        E maxE = maximum(root).val;
+        removeMax(root);
+        return maxE;
     }
 
     private Node removeMax(Node node) {
@@ -253,13 +255,64 @@ public class BinarySearchTree<E extends Comparable> {
         return node;
     }
     private Node maximum(Node node){
-        if (node.left == null) {
-            return node.left;
+        if (node.right == null) {
+            return node;
         }
-        return minimum(node.left);
+        return maximum(node.right);
     }
-    public void remove(){
+    public void remove(E e){
+        /**
+         * 删除的动作，难点在于要删除的节点是既有左子树又有右子树的节点
+         * 此时我们需要重新考虑各个节点间序的关系(需要左右子树的融合)
+         *      方法是，找到删除节点的后继节点（或者前驱节点）：Hibbard deletion
+         *          后继节点：比当前节点大，且距离当前节点最近的节点（右子树的最小值）
+         *          前驱节点：比当前节点小，且距离当前节点最近的节点（左子树的最大值）
+         *
+         *      而带删除的节点是该节点右子树的最小值
+         */
+        root = remove(root, e);
+    }
+    // 递归函数
+    private Node remove(Node node, E e){
+        /**
+         * 删除以 node 为跟节点的 二分搜索树 的最小节点
+         * 然后返回删除后的新二分搜索树的根
+         */
+        if (node == null) {
+            return null;
+        }
 
+        if (e.compareTo(node.val) < 0){
+            node.left = remove(node.left, e);
+            return node;
+        } else if (e.compareTo(node.val) > 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else{
+            if (node.left == null){  // 只有右子树的时候，根删除最小值一样
+                Node rightChildren = node.right;
+                node.right = null;
+                size--;
+                return rightChildren;
+            }
+            if (node.right == null){  // 只有左子树的时候，根删除最大值一样
+                Node leftChildren = node.left;
+                node.left = null;
+                size--;
+                return leftChildren;
+            }
+
+            // 待删除节点左右子树均不为空时：找到比待删除节点大（小）的最小值（最大值），即待删除节点右（左）子树的最小值（最大值）
+            // 然后用这个节点代替待删除节点，并删除最小值
+            Node successor = minimum(node.right);
+            // 重新构建左右子树
+            successor.right = removeMin(node.right);  // 将待删除节点右子树删除最小值后的新树返回给删除节点的右子树
+            successor.left = node.left;
+
+            // 删除待删除节点的指向(这里才是对待删除节点真正的删除动作)
+            node.left = node.right = null;
+            return successor;
+        }
     }
 
     private class Node {
@@ -280,13 +333,28 @@ public class BinarySearchTree<E extends Comparable> {
         for (int i : arr) {
             bst.add(i);
         }
+        System.out.println("=================================TEST TRAVERSE==============================");
         System.out.print("前序遍历：");
         bst.preOrder();
-        System.out.println("");
+        System.out.print("\n非递归前序遍历： ");
         bst.traverse();
-        System.out.printf("");
-
+        System.out.println("");
+        
+        System.out.println("=================================TEST CONTAINS==============================");
         System.out.println("contains 58: " + bst.contains2(58));
         System.out.println("contains 43: " + bst.contains2(43));
+
+        System.out.println("=================================TEST REMOVE==============================");
+        List<Integer> removeOrder = new ArrayList<>();
+        while (!bst.isEmpty()){
+            removeOrder.add(bst.removeMin());
+        }
+        System.out.println(removeOrder.toString());
+        for (int i = 1; i < removeOrder.size(); i++) {
+            if (removeOrder.get(i-1) > removeOrder.get(i)){
+                throw new IllegalArgumentException("ERROR");
+            }
+        }
+        System.out.println("SUCCESS");
     }
 }
